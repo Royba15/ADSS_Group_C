e48e9c8549eacb4fad5c64a7960de62d3662bcb1
yuval-asulin <yuval3553@gamil.com>
2026-04-13 18:13:50 +0300

    worker fields

---
+++ dev/src/main/java/HR/domain/Worker.java
@@ -0,0 +1,18 @@
+package domain;
+
+public class Worker {
+    //describes a worker in the company
+    private
+    String name;
+    int id;
+    int bank_no;
+    float salary;
+    String start_date;
+    String end_date;
+    String job_scope;
+    String salary_type;
+    String day_off = "Saturday";
+    Boolean is_manager;
+    String[] skills;
+
+}
