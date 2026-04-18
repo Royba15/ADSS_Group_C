4641ca4a9f0f6ff24965f893ee744911b565d1a3
yuval-asulin <yuval3553@gamil.com>
2026-04-18 15:19:25 +0300

    Add dev source and tests

--- dev/Tests/Test2.java
+++
@@ -1,6 +0,0 @@
-public class Test2 {
-
-    public static void main(String[] args) throws Exception {
-        TestSupport.runTest("in2.txt", "out2.txt");
-    }
-}
--- dev/Tests/Test3.java
+++
@@ -1,6 +0,0 @@
-public class Test3 {
-
-    public static void main(String[] args) throws Exception {
-        TestSupport.runTest("in3.txt", "out3.txt");
-    }
-}
